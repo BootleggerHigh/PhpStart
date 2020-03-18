f9373c0784205ff47ddcd78e8ef346bfdfdb4504
Dmitry <bootleggerlow@gmail.com>
2020-03-18 23:11:32 +0300

    chapter 15

---
+++ chapter 15/second-task/PageDir.php
@@ -0,0 +1,26 @@
+<?php
+
+
+class PageDir extends Pager
+{
+
+    protected function pageLink()
+    {
+        // TODO: Implement pageLink() method.
+    }
+
+    protected function pnumber()
+    {
+        // TODO: Implement pnumber() method.
+    }
+
+    protected function parameters()
+    {
+        // TODO: Implement parameters() method.
+    }
+
+    protected function total()
+    {
+        // TODO: Implement total() method.
+    }
+}
---
+++ chapter 15/second-task/PagerDb.php
@@ -0,0 +1,25 @@
+<?php
+
+
+class PagerDb extends Pager
+{
+    protected function pageLink()
+    {
+        // TODO: Implement pageLink() method.
+    }
+
+    protected function pnumber()
+    {
+        // TODO: Implement pnumber() method.
+    }
+
+    protected function parameters()
+    {
+        // TODO: Implement parameters() method.
+    }
+
+    protected function total()
+    {
+        // TODO: Implement total() method.
+    }
+}
